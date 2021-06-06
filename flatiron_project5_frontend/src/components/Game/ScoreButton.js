2b34f70229668f5bd2ac741203be3fec14698c5a
jinook929 <jinook929@gmail.com>
2021-06-05 20:46:23 -0400

    clean up ScoreButton.js

--- flatiron_project5_frontend/src/components/Game/ScoreButton.js
+++ flatiron_project5_frontend/src/components/Game/ScoreButton.js
@@ -1,6 +1,8 @@
+// React related
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+// Material UI related
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,24 +19,13 @@ const useStyles = makeStyles((theme) => ({
 
 const ScoreButton = (props) =>  {
   const classes = useStyles();
-
   return (
     <div className={classes.root}>
-      {/* <Button variant="outlined">Default</Button>
-      <Button variant="outlined" color="primary">
-        Primary
-      </Button>
-      <Button variant="outlined" color="secondary">
-        Secondary
-      </Button> */}
       <Button variant="outlined" color="default" className={classes.button}>
         Current Score: {props.score * 100}
       </Button>
-      {/* <Button variant="outlined" color="primary" href="#outlined-buttons">
-        Link
-      </Button> */}
     </div>
-  );
+  )
 }
 
 export default ScoreButton
